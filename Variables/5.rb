5e673e067abdb32152379cb717e07b27a9cdf314
Josh Edmondson <joshedmondson@protonmail.com>
2023-06-13 19:05:11 -0600

    Finsihed the variables section along with the exercises

---
+++ Variables/5.rb
@@ -0,0 +1,5 @@
+=begin
+In the first case x prints 3.
+In the second case puts x throws an error as x was declared with-in the scope of the 3.times method.
+  
+=end
---
+++ Variables/name.rb
@@ -0,0 +1,6 @@
+
+puts "Please Enter your Name:"
+name = gets.chomp
+
+10.times {puts "Hello #{name}!"}
+
